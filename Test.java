e3ae4477a988cc223bdb9e2bbc560e57716a0c95
342vijay <159497647+342vijay@users.noreply.github.com>
2026-03-16 09:46:59 +0530

    Remove add method and update main method output
    Removed the add method and updated main method to print 'HelloWorld'.

--- Test.java
+++ Test.java
@@ -1,15 +1,9 @@
 public class Test {
 
-  static void add(){
-
-    int a=5;
-    int b=15;
-    int result = a+b;
-    System.out.println("The sum is " +result);
-    
-  }
+  
+  
   public static void main(String[] args)
   {
-    add();
+    System.out.println("HelloWorld");
   }
 }
